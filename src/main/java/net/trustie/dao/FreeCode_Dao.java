9373ca275af436ecfc17e7c054024729bc694513
rails -v <nudt_houxiang@163.com>
2015-02-03 19:28:34 +0800

    final

--- src/main/java/net/trustie/dao/FreeCode_Dao.java
+++ src/main/java/net/trustie/dao/FreeCode_Dao.java
@@ -6,7 +6,7 @@ import net.trustie.model.FreeCode_Model;
 
 
 public interface FreeCode_Dao {
-	@Insert("insert into openhub_html_detail"
+	@Insert("insert into freecode_projects"
 			+"(`project_url`,`extract_time`,`tag`,`license`,`operate_system`,`implementation`,`project_title`,`project_des`,`author`,`author_url`,`post_date`,`project_downloads`,`project_website`,`crawlerTime`)"
 			+" values (#{projectUrl},#{extractTime},#{tag},#{license},#{operateSystem},#{implementation},#{projectTitle},#{projectDesc},#{author},#{authorUrl},#{postDate},#{projectDownloads},#{projectWebsite},#{crawlerTime})") 
 	public int add(FreeCode_Model fmodel);
